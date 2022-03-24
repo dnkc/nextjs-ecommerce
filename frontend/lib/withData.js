1751cce68ae8f94f334724556ea4a0687b008811
dnkc <dnkcdesign@gmail.com>
2022-03-24 15:20:50 -0400

    trying to fix git bug

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -5,6 +5,7 @@ import { createUploadLink } from "apollo-upload-client";
 import withApollo from "next-with-apollo";
 import { endpoint, prodEndpoint } from "../config";
 import paginationField from "./paginationField";
+//pls
 
 function createClient({ headers, initialState }) {
   return new ApolloClient({
